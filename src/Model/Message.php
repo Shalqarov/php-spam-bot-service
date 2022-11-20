cba1b28357a8d0786e0ecceb742a9110532735b0
Shalqarov <shalqarov01@gmail.com>
2022-11-20 18:40:27 +0600

    Добавлена модель сообщений

--- src/Model/Message.php
+++ src/Model/Message.php
@@ -2,7 +2,45 @@
 
 namespace App\Model;
 
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\Table;
+
+#[Entity]
+#[Table('adverts')]
 class Message
 {
-    // TODO
+    #[Id]
+    #[Column, GeneratedValue]
+    private ?int $id;
+
+    #[Column(type: Types::STRING)]
+    private ?string $message;
+
+    public function __construct($data = [])
+    {
+        $this->id = $data['id'] ?? null;
+        $this->message = $data['message'] ?? null;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message ?? '';
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getId(),
+            'message' => $this->getMessage(),
+        ];
+    }
 }
