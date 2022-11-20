f1120dd266c432e6ae80bb47d9e79997a1807899
Shalqarov <shalqarov01@gmail.com>
2022-11-20 23:11:22 +0600

    добавлен пост запрос в бота

--- src/Http/Controllers/MessageController.php
+++ src/Http/Controllers/MessageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Services\Service;
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Request;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Http\Response;
 use Slim\Http\ServerRequest;
@@ -22,10 +24,29 @@ class MessageController
         return Twig::fromRequest($request)->render($response, 'message.twig');
     }
 
-    public  function sendMessage(ServerRequest $request, Response $response): ResponseInterface
+    public function sendMessage(ServerRequest $request, Response $response): ResponseInterface
     {
         $data = $request->getParsedBodyParam('message', []);
+        $msg = $data['message'] . "\\n#aboba_team";
+        try {
+            $this->send($msg);
+        }catch (\Exception $e) {
+            die(500);
+        }
         $this->service->setItem($data);
         return $response->withRedirect('/history');
+    }
+
+    private function send(string $message)
+    {
+        $client = new Client();
+        $headers = [
+            'Content-Type' => 'application/json'
+        ];
+        $body = "{
+             \"message\": \"{$message}\"
+        }";
+        $req = new Request('POST', 'localhost:5000/api/send', $headers, $body);
+        $client->sendAsync($req)->wait();
     }
 }
